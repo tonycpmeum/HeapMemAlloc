1f155acf500a1e2fda1c7cf0460d67f7afaedf35
Tony?? <tonycpmeum@gmail.com>
2024-02-21 22:14:07 +0800

    heap allocation complete, bitches

--- app/headers/memory.h
+++ app/headers/memory.h
@@ -11,15 +11,29 @@ struct {
    void *ptr;
 } _HEAP_DATA = { NULL };
 
-void update_block_data(void *ptr, uint32_t block_size, bool allocated) {
+void _update_block_data(void *ptr, uint32_t block_size, bool allocated) {
    uint32_t *start = (uint32_t*)ptr;
    uint32_t *end = ptr;
    // header size = 4 bytes, at the start + end of block
    // block size = payload + padding + 2x 4 byte headers
    DISPLACE_PTR(uint32_t*, end, block_size - 4);
 
+   uint32_t prev_bdata = *start;
    *start = block_size | allocated;
    *end = block_size | allocated;
+
+   if (block_size == HEAP_SIZE) { return; }
+
+   // update affected neighboring block datas
+   if (allocated) {
+      DISPLACE_PTR(uint32_t*, start, block_size);
+      *start = prev_bdata - block_size;
+
+      DISPLACE_PTR(uint32_t*, end, prev_bdata - block_size);
+      *end = prev_bdata - block_size;
+   } else {
+
+   }
 }
 
 void _heap_init(void) {
@@ -30,21 +44,38 @@ void _heap_init(void) {
       exit(1);
    }
 
-   update_block_data(start, HEAP_SIZE, 0);
    _HEAP_DATA.ptr = start;
+   _update_block_data(start, HEAP_SIZE, 0);
 }
 
-void *heap_alloc(uint8_t size) {
+void *_find_free_block(uint32_t OGblock_size) {
+   char *curr_block;
+   uint32_t block_data, block_size = 0;
+   do {
+      curr_block = (char*)_HEAP_DATA.ptr + block_size;
+      if (curr_block > (char*)_HEAP_DATA.ptr + HEAP_SIZE) {
+         printf("uhm\n");
+         exit(1);
+      }
+      block_data = *(uint32_t*)curr_block;
+      // 3 LSB used as trigger bits
+      block_size = block_data & 0xfffffff8;
+   // LSB == 1: allocated; LSB == 0: free;
+   } while (block_data & 1 || OGblock_size > block_size);
+
+   return (void*)curr_block;
+}
+
+void *heap_alloc(uint32_t payload) {
    if (_HEAP_DATA.ptr == NULL) { _heap_init(); }
 
    // block size = payload + padding + 2x 4 byte headers
-   size_t block_size = 8 + ( size / 8 + (size % 8 != 0) ) * 8;
-   printf("%ld\n", block_size);
+   size_t block_size = 8 + ( payload / 8 + (payload % 8 != 0) ) * 8;
 
-   // find free block with enough space first;
+   void *ptr = _find_free_block(block_size);
+   _update_block_data(ptr, block_size, true);
 
-
-   return _HEAP_DATA.ptr;
+   return ptr;
 }
 
 void heap_free(void *ptr);
