3655b19e16abea1a8c634458bade73579221cdf7
Tony?? <tonycpmeum@gmail.com>
2024-02-22 15:39:48 +0800

    heap_free & some bug left to solve im lazy

--- app/headers/memory.h
+++ app/headers/memory.h
@@ -4,12 +4,13 @@
 
 #define HEAP_SIZE sysconf(_SC_PAGE_SIZE) * 10
 #define HEAP_FAILURE (void*)-1
-#define ALLOC_STAT(x) ((x & 1) == true)
-#define DISPLACE_PTR(ptr_type, ptr, byte) (ptr = (ptr_type) ((char*)ptr + byte))
+#define DISPLACE_PTR(ptr_type, ptr, byte) (ptr = (ptr_type) ((char*)ptr + (byte)))
 
 struct {
    void *ptr;
 } _HEAP_DATA = { NULL };
+
+#define DISPLACE_STRT_PTR(byte) ((void*) (((char*)_HEAP_DATA.ptr + (byte))))
 
 void _update_block_data(void *ptr, uint32_t block_size, bool allocated) {
    uint32_t *start = (uint32_t*)ptr;
@@ -21,19 +22,38 @@ void _update_block_data(void *ptr, uint32_t block_size, bool allocated) {
    uint32_t prev_bdata = *start;
    *start = block_size | allocated;
    *end = block_size | allocated;
+   printf("Header -> %d\n", *start);
 
    if (block_size == HEAP_SIZE) { return; }
 
-   // update affected neighboring block datas
    if (allocated) {
+      // update separated free block 
       DISPLACE_PTR(uint32_t*, start, block_size);
       *start = prev_bdata - block_size;
 
       DISPLACE_PTR(uint32_t*, end, prev_bdata - block_size);
       *end = prev_bdata - block_size;
    } else {
+      if (start != _HEAP_DATA.ptr) {
+         // coalescing free blocks, x = prev; y = next;
+         uint32_t x_block_data = *(start - 1);
+         if ((x_block_data & 1) == false) {
+            DISPLACE_PTR(uint32_t*, start, -1 * x_block_data);
+            *start += *end;
+            *end += *start;
+         }
+      }
 
+      if (end + 1 != DISPLACE_STRT_PTR(HEAP_SIZE)) {
+         uint32_t y_block_data = *(end + 1);
+         if ((y_block_data & 1) == false) {
+            DISPLACE_PTR(uint32_t*, end, y_block_data);
+            *start += *end;
+            *end += *start;
+         }
+      }
    }
+
 }
 
 void _heap_init(void) {
@@ -46,22 +66,30 @@ void _heap_init(void) {
 
    _HEAP_DATA.ptr = start;
    _update_block_data(start, HEAP_SIZE, 0);
+
+   printf("--- Heap init ---\n\n");
 }
 
 void *_find_free_block(uint32_t OGblock_size) {
    char *curr_block;
    uint32_t block_data, block_size = 0;
+
+   // stuck here
    do {
-      curr_block = (char*)_HEAP_DATA.ptr + block_size;
-      if (curr_block > (char*)_HEAP_DATA.ptr + HEAP_SIZE) {
+      curr_block = DISPLACE_STRT_PTR(block_size);
+      printf("O BLOCKSIZE %d\n", block_size);
+      printf("AT BLOCK %p\n", curr_block);
+      if ((void*)curr_block > DISPLACE_STRT_PTR(HEAP_SIZE)) {
          printf("uhm\n");
          exit(1);
       }
-      block_data = *(uint32_t*)curr_block;
       // 3 LSB used as trigger bits
-      block_size = block_data & 0xfffffff8;
+      block_data = *(uint32_t*)curr_block;
+      block_size = block_data & 0xFFFFFFF8;
    // LSB == 1: allocated; LSB == 0: free;
    } while (block_data & 1 || OGblock_size > block_size);
+
+   printf("found %p\n", curr_block);
 
    return (void*)curr_block;
 }
@@ -71,11 +99,21 @@ void *heap_alloc(uint32_t payload) {
 
    // block size = payload + padding + 2x 4 byte headers
    size_t block_size = 8 + ( payload / 8 + (payload % 8 != 0) ) * 8;
-
+   printf("block size: %ld\n", block_size);
+   
    void *ptr = _find_free_block(block_size);
    _update_block_data(ptr, block_size, true);
 
    return ptr;
 }
 
-void heap_free(void *ptr);
+void heap_free(void *ptr) {
+   if (ptr < _HEAP_DATA.ptr || ptr > DISPLACE_STRT_PTR(HEAP_SIZE)) {
+      printf("bonk\n");
+      exit(1);
+   }
+
+   uint32_t block_data = *(uint32_t*)ptr;
+   
+   _update_block_data(ptr, block_data & 0xFFFFFFF8, block_data & 1);
+}
--- app/source.c
+++ app/source.c
@@ -2,12 +2,25 @@
 #include "headers/memory.h"
 
 int main() {
-   char* ptr = heap_alloc(10);
-   // printf("ptr = %p\n", ptr);
-
-   // uint32_t val = *(uint32_t*)(ptr - 4);
-   // printf("Heap mem size = %d\n", val);
+   char *ptr = heap_alloc(16);
+   printf("%p\n\n", ptr);
+   int *inte = heap_alloc(20);
+   printf("%p\n\n", inte);
    
+   heap_free(ptr);
+   
+   uint64_t *bigmf = heap_alloc(64);
+   printf("%p\n\n", bigmf);
+
+   heap_free(inte);
+
+   int *ganggang = heap_alloc(8);
+   printf("%p\n\n", ganggang);
+
+   heap_free(bigmf);
+   heap_free(ganggang);
+
+   munmap(_HEAP_DATA.ptr, HEAP_SIZE);
 
    return 0;
 }
